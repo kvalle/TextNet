a369119c61da6e2b9cd12517592a110b770a5b7b
Kjetil Valle <kjetil.valle@gmail.com>
2011-03-30 12:02:03 +0200

    some preliminary tests

--- higher_order_co_occ_experiments.py
+++ higher_order_co_occ_experiments.py
@@ -12,7 +12,27 @@ import scipy.spatial.distance
 
 numpy.set_printoptions(linewidth = 1000, precision = 3)
 
+def test():
+    path = '../data/air/problem_descriptions_preprocessed'
+    texts, labels = data.read_files(path)
+    lengths = []
+    for i, text in enumerate(texts):
+        print str(i)+"/"+str(len(texts))
+        g = graph_representation.construct_higher_order_cooccurrence_network(text, 3)
 
+def test_vocabulary_size():
+    path = '../data/air/problem_descriptions_preprocessed'
+    texts, labels = data.read_files(path)
+    lengths = []
+    for text in texts:
+        text = text.split(' ')
+        l = len(list(set(text)))
+        lengths.append(l)
+        print '   ',l
+    lengths = numpy.array(lengths)
+    print 'avg', lengths.mean()
+    print 'max', lengths.max()
+    print 'min', lengths.min()
 
 if __name__ == "__main__":
-    pass
+    test()
