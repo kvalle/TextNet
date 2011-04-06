26d7963665ad8234c15777c025056b9d6f4d8f0c
Kjetil Valle <kjetil.valle@gmail.com>
2011-04-06 11:40:42 +0200

    added init file

---
+++ __init__.py
@@ -0,0 +1,14 @@
+import data
+import graph
+import classify
+import representation
+import util
+import selector
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
+__all__ = ['data', 'graph', 'classify', 'util'] # TODO: update
