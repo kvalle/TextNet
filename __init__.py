f0385bfb29152c8739e83ddb0fe626289d856411
Kjetil Valle <kjetil.valle@gmail.com>
2011-04-08 10:48:36 +0200

    cleaned up init file

--- __init__.py
+++ __init__.py
@@ -1,12 +1,1 @@
-import data
-import graph
-import classify
-import util
 
-def main():
-    pass
-
-if __name__ == "__main__":
-    main()
-
-__all__ = ['data', 'graph', 'classify', 'util'] # TODO: update
