4a386e5bbddaaecefef9832f5a7438458b75d817
Kjetil Valle <kjetil.valle@gmail.com>
2011-05-03 20:22:36 +0200

    extended report crawler script

--- report_scraper.py
+++ report_scraper.py
@@ -6,15 +6,38 @@ import os
 
 def fetch(url):
     opener = urllib2.build_opener()
-    opener.addheaders = [("User-agent", "report-crawler-bot")]
+    opener.addheaders = [("User-agent", "report-crawler")]
     response = opener.open(url)
     return response.read()
 
+def fetch_extended_report(report_url):
+    alt_url = report_url.replace('.asp','_index.asp')
+    try:
+        doc = fetch(alt_url).strip()
+    except urllib2.HTTPError as e:
+        # not a problem, just no alternate for this report
+        return False
+    sec_num = 1
+    while True:
+        alt_url = report_url.replace('.asp','_sec'+str(sec_num)+'.asp')
+        try:
+            sec = fetch(alt_url).strip()
+            doc += '\n\n'+sec
+        except urllib2.HTTPError as e:
+            # 404, no more sections
+            break
+        sec_num += 1
+    return doc
+
 def store_report(report_url, save_dir):
+    alt_url = report_url.replace('.asp','_index.asp')
     name = report_url.rpartition('/')[2]
     name = name.replace('.asp','.html')
     print 'Fetching',name
     doc = fetch(report_url).strip()
+    alt = fetch_extended_report(report_url)
+    if alt:
+        doc += '\n\n'+alt
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
     filename = save_dir+'/'+name
@@ -30,17 +53,38 @@ def extract_report_urls(index):
             urls.append(a['href'])
     return urls
 
-def crawl_reports(year):
-    output_dir = '../data/rir/reports/'+str(year)
-    base_url = 'http://www.tsb.gc.ca/eng/rapports-reports/rail/'+year+'/'
+def crawl_reports(url, year, output_dir):
+    base_url = url+year+'/'
     index = fetch(base_url).strip()
     report_urls = extract_report_urls(index)
     report_urls = [base_url+rel for rel in report_urls]
-    for url in report_urls:
-        store_report(url, output_dir)
+    for r_url in report_urls:
+        store_report(r_url, output_dir+'/'+year)
 
-if __name__=='__main__':
+def fetch_rail():
     cats = ['precedentes-earlier','etudes-studies']
     cats += [str(y) for y in range(1994,2011)]
+    base_url = 'http://www.tsb.gc.ca/eng/rapports-reports/rail/'
+    data_dir = '../data/rir/reports/'
     for c in cats:
-        crawl_reports(c)
+        crawl_reports(base_url, c, data_dir)
+
+def fetch_marine():
+    cats = ['precedentes-earlier','etudes-studies']
+    cats += [str(y) for y in range(1994,2010)]
+    base_url = 'http://www.tsb.gc.ca/eng/rapports-reports/marine/'
+    data_dir = '../data/mir/reports/'
+    for c in cats:
+        crawl_reports(base_url, c, data_dir)
+
+def fetch_pipeline():
+    cats = ['1994','1995','1996','1997','1999','2000','2001','2002','2005','2006', '2007','2009']
+    base_url = 'http://www.tsb.gc.ca/eng/rapports-reports/pipeline/'
+    data_dir = '../data/pir/reports/'
+    for c in cats:
+        crawl_reports(base_url, c, data_dir)
+
+if __name__=='__main__':
+    fetch_marine()
+    fetch_rail()
+    fetch_pipeline()
