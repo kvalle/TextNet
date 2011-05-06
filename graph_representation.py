8a4eb8503d3074ab263f4a154ef1a2c4f6eff74a
Kjetil Valle <kjetil.valle@gmail.com>
2011-05-06 15:26:05 +0200

    retr comparison evaluation

--- graph_representation.py
+++ graph_representation.py
@@ -104,7 +104,7 @@ def _sentence_cooccurrence_matrix(doc, direction='undirected'):
                 A[x,y] += 1
     return A, term_list
 
-def construct_cooccurrence_network(doc, window_size=2, direction='undirected', context='window', already_preprocessed=False, orders=[], order_weights=[1.0,1.0,1.0],doc_id=None):
+def construct_cooccurrence_network(doc, window_size=2, direction='undirected', context='sentence', already_preprocessed=False, orders=[], order_weights=[1.0,1.0,1.0],doc_id=None):
     """Construct co-occurrence network from text.
 
     *direction* must be 'forward', 'backward' or 'undirected', while  *context*
