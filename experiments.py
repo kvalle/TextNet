024e83643b376a55f8bacaf1e83c0edfe01511ee
Kjetil Valle <kjetil.valle@gmail.com>
2011-05-08 16:59:45 +0200

    misc

--- experiments.py
+++ experiments.py
@@ -365,7 +365,7 @@ def solution_similarity_stats(dataset='air/solutions_preprocessed'):
     similarities = 1.0 - distances
     similarities = similarities.ravel()
     similarities = [s for s in similarities if s >= 0]
-    #~ print plotter.histogram(similarities,'similarity','# matches','',bins=150)
+    print plotter.histogram(similarities,'similarity','# matches','',bins=150)
     print
     print max(similarities)
     print min(similarities)
@@ -402,9 +402,11 @@ if __name__ == "__main__":
     #~ classification_comparison_graph(graph_type='dependency', icc=True)
     #~ classification_comparison_freq()
 
-    retrieval_comparison_graph(graph_type='co-occurrence', icc=True)
+    #~ retrieval_comparison_graph(graph_type='co-occurrence', icc=True)
     #~ retrieval_comparison_graph(graph_type='dependency', icc=False)
     #~ retrieval_comparison_freq()
 
     #~ test_document_lengths()
     #~ solution_similarity_stats(dataset='mir/solutions_preprocessed')
+
+    solution_similarity_stats()
